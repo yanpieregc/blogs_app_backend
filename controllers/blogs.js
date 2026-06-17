0c9fb5ad62be85a580e59871f929341d1361463c
unknown <yanpieregc@gmail.com>
2026-06-17 18:01:40 -0500

    actualizacion del metodo put

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -48,13 +48,14 @@ blogsRouter.put('/:id', middleware.userExtractor, async (req, res) => {
     return res.status(404).json({ error: 'blog not found' })
   }
 
-  if (blog.user.toString() !== user._id.toString()) {
-    return res.status(403).json({ error: "forbidden" })
-  } 
+  // if (blog.user.toString() !== user._id.toString()) {
+  //   return res.status(403).json({ error: "forbidden" })
+  // } 
 
   blog.likes = likes
-  const updateBlog = await blog.save()
-  res.json(updateBlog)
+  await blog.save()
+  await blog.populate('user', { username: 1, name: 1 })
+  res.json(blog)
 })
 
 blogsRouter.delete('/:id', middleware.userExtractor, async (req, res) => {
